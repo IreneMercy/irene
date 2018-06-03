4408ca35e55f64e364e66ee5a079bfd9976beb5a
IreneMercy <irenemercy700@gmail.com>
2018-06-02 22:44:43 -0700

    first commit

---
+++ irene.py
@@ -0,0 +1,12 @@
+fruits_list = ['mango', 'orange', 'banana',]
+
+students_list = [1, 2, 3, 4, 5, 6]
+
+
+jane_list = ['Jane', 20, 'Nairobits']
+
+color_tuple = ('red', 'orange', 'pink')
+
+my_dict = {'Name':'Irene', 'Age':20, 'School':'Nairobits'}
+
+my_set = set("Name", "age", "school", "house")
